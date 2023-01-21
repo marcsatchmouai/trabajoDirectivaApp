3c25adea20d6499cf893ea08cc17ed0b06f329c0
Marc Satchmo <marcsatchmo@gmail.com>
2023-01-20 21:00:53 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = "Registro de Usuario";
+  mensaje:string="";
+  registrado:boolean=false;
+  nombre:string="";
+  apellido:string="";
+  //entradas:any[];
+  entradas:Array<{titulo:string}>
+
+  constructor(){
+    this.entradas=[
+      {titulo:"Python cada día mas presente"},
+      {titulo:"Java presente desde hace 20 años"},
+      {titulo:"Javascript cada dia mas funcional"},
+      {titulo:"Kotilin potencia para tus apps"},
+      {titulo:"Donde quedo Pascal?"},
+      {titulo:"Prueba"}
+
+    ]
+  }
+
+  registrarUsuario():void{
+    this.registrado=true;
+    this.mensaje = "usuario registrado con exito";
+  }
 }
